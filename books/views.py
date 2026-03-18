e0b9c9b6fa71a6cb770680045592a42fb9e4bc31
Andrii Yarotskiy <andriiyarotskiy@gmail,com>
2026-03-18 10:32:56 +0000

    Added permissions for books

--- books/views.py
+++ books/views.py
@@ -1,4 +1,5 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, permissions
+from rest_framework.permissions import AllowAny
 
 from books.models import Book
 from books.serializers import BookSerializer
@@ -7,3 +8,9 @@ from books.serializers import BookSerializer
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+    permission_classes = (AllowAny,)
+
+    def get_permissions(self):
+        if self.action in ("destroy", "update", "partial_update", "create"):
+            return [permissions.IsAuthenticated(), permissions.IsAdminUser()]
+        return super().get_permissions()
